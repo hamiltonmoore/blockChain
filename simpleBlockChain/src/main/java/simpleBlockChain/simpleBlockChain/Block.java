e5caafd2c4df1cbb893d0aa09277515bae5d0a47
hamilton moore <hamlmoor@gmail.com>
2018-02-10 15:57:28 -0600

    recreated project structure using spring

---
+++ simpleBlockChain/src/main/java/simpleBlockChain/simpleBlockChain/Block.java
@@ -0,0 +1,12 @@
+package simpleBlockChain.simpleBlockChain;
+
+public class Block {
+
+    private int previousHash;
+    private int currentHash;
+    private String data;
+
+    Block(int previousHash, int currentHash, String data) {
+        
+    }
+}
---
+++ simpleBlockChain/src/main/java/simpleBlockChain/simpleBlockChain/SimpleBlockChainApplication.java
@@ -0,0 +1,12 @@
+package simpleBlockChain.simpleBlockChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlockChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlockChainApplication.class, args);
+	}
+}
